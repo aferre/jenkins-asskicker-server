b3ad2aaf1c0048f42382d928e6f4210c2f735849
Adrien Ferre <ferre.adrien@gmail.com>
2013-09-01 12:18:02 +0200

    loading conf

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ var fs = require('fs');
 var speakerQueue = require('./speakerQueue');
 var tts = require('./tts');
 var jenkins = require('./jenkins');
+var nconf = require('nconf');
+var dic = require('nconf');
 
-var grettingsDictionary = {
-    'bled': 'ouech ca va ou quoi?',
-    'poli': 'Bonjour comment allez vous?'
-};
+  nconf.argv()
+       .env()
+       .file({ file: 'config.json' });
+       
+       
+  dic.argv()
+       .env()
+       .file({ file: 'dictionary.json' });
+       
+var grettingsDictionary = dic.get("greetings");
 
 function randomValue(data) {
     var rand = Math.floor(Math.random() * (Object.keys(data).length));
@@ -16,10 +24,10 @@ function randomValue(data) {
 function greetings(endCallback) {
     var rand = randomValue(grettingsDictionary);
     if (endCallback) {
-        tts.retrieve(rand, 'fr', endCallback);
+        tts.retrieve(rand, 'en', endCallback);
     }
     else {
-        tts.retrieve(rand, 'fr', retrievedTTS);
+        tts.retrieve(rand, 'en', retrievedTTS);
     }
 }
 
